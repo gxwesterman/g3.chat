30cc63bd329eeff9b0ab05e5e9d9c5aff22b1226
Griffin Westerman <gxweste@gmail.com>
2025-02-26 10:42:18 -0600

    feat: set up active sidebarmenuitems

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,8 +1,14 @@
 import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "@/components/ui/sidebar"
 import { db } from '@/lib/instant';
 import Link from 'next/link';
+import { usePathname } from "next/navigation";
+import { useState } from "react";
  
 export function AppSidebar() {
+
+  const pathname = usePathname();
+  const pageChatId = (pathname.split('/').pop() || '');
+  const [activeChatId, setActiveChatId] = useState(pageChatId);
 
   const { isLoading, error, data } = db.useQuery({ chats: {} });
   if (isLoading) return <div></div>;
@@ -11,13 +17,13 @@ export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarContent>
-        <SidebarHeader>
+        <SidebarHeader className="pt-5 px-3">
           <Link href='/chat'>G3 Chat</Link>
         </SidebarHeader>
-        <SidebarMenu>
+        <SidebarMenu className="px-3">
           {data.chats.map((chat) => (
             <SidebarMenuItem key={chat.id}>
-              <SidebarMenuButton>
+              <SidebarMenuButton isActive={activeChatId === chat.id} asChild className="py-5" onClick={() => setActiveChatId(chat.id)}>
                 <Link href={`/chat/${chat.id}`} key={chat.id}>New Chat</Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
