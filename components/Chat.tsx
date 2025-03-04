8b1aa7be2a62ff8d5497679a45e36278acbe4ed1
Griffin Westerman <gxweste@gmail.com>
2025-03-04 13:10:20 -0600

    feat: refactored everything - put mostly everything into the Chat component

---
+++ components/Chat.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { db } from "@/lib/instant";
+import ChatForm from '@/components/ChatForm';
+import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm";
+
+export default function Chat() {
+
+  const pathname = usePathname();
+  const pageChatId = pathname.split('/').pop() || '';
+  const [output, setOutput] = useState('');
+
+  const messagesQuery = {
+    $: {
+      where: {
+        chatId: pageChatId
+      }
+    }
+  };
+
+  const { isLoading, error, data } = db.useQuery({ messages: messagesQuery });
+  if (isLoading) return <div></div>;
+  if (error) return <div>Error fetching data: {error.message}</div>;
+
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="relative flex w-full flex-1 flex-col h-[100dvh]">
+        <SidebarTrigger />
+        <div className="relative flex-1 overflow-hidden">
+          <ChatForm messages={data.messages} output={output} setOutput={setOutput} />
+          <div className="scrollbar scrollbar-w-2 scrollbar-thumb-gray-700 scrollbar-track-transparent hover:scrollbar-thumb-gray-600 h-[100dvh] overflow-y-auto pb-[140px]">
+            <div className="mx-auto flex w-full max-w-3xl flex-col space-y-12 p-4 pb-8">
+              {data.messages.map((message) => (
+                  <div key={message.id}>
+                      {
+                          message.type === 'question' ? 
+                          (
+                              <div className="flex justify-end">
+                                  <div className="group relative inline-block max-w-[80%] break-words rounded bg-secondary p-4 text-left">
+                                      <div>{message.text}</div>
+                                  </div>
+                              </div>
+                          )
+                      :
+                          (
+                              <div className="flex justify-start chat-content">
+                                  <div className="group relative w-full max-w-full break-words">
+                                      <div className="space-y-4 prose prose-neutral prose-invert max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0">
+                                        <ReactMarkdown remarkPlugins={[remarkGfm]}>{message.text}</ReactMarkdown>
+                                      </div>
+                                  </div>
+                              </div>
+                          )
+                      }
+                  </div>
+              ))}
+              <div className="flex justify-start chat-content">
+                  <div className="group relative w-full max-w-full break-words">
+                      <div className="space-y-4 prose prose-neutral prose-invert max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0">
+                        <ReactMarkdown remarkPlugins={[remarkGfm]}>{output}</ReactMarkdown>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </SidebarProvider>
+  );
+}
