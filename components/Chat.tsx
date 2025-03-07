6e59d3223236e0c431ed906e1d60229ce3f11948
Griffin Westerman <gxweste@gmail.com>
2025-03-07 15:57:26 -0600

    chore: formatting

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -25,47 +25,45 @@ export default function Chat() {
   if (error) return <div>Error fetching data: {error.message}</div>;
 
   return (
-    
-        <div className="relative flex-1 overflow-hidden">
-          <ChatForm messages={data.messages} output={output} setOutput={setOutput} />
-          <div className="scrollbar scrollbar-w-2 scrollbar-thumb-gray-700 scrollbar-track-transparent hover:scrollbar-thumb-gray-600 h-[100dvh] overflow-y-auto pb-[140px]">
-            <div className="mx-auto flex w-full max-w-3xl flex-col space-y-12 p-4 pb-8 text-foreground/80">
-              {!isLoading ? 
-                data.messages.map((message) => (
-                    <div key={message.id}>
-                        {
-                            message.type === 'question' ? 
-                            (
-                                <div className="flex justify-end">
-                                    <div className="group relative inline-block max-w-[80%] break-words rounded bg-background shadow-sm shadow-background p-4 text-left">
-                                        <div>{message.text}</div>
+    <div className="relative flex-1 overflow-hidden">
+      <ChatForm messages={data.messages} output={output} setOutput={setOutput} />
+      <div className="scrollbar scrollbar-w-2 scrollbar-thumb-gray-700 scrollbar-track-transparent hover:scrollbar-thumb-gray-600 h-[100dvh] overflow-y-auto pb-[140px]">
+        <div className="mx-auto flex w-full max-w-3xl flex-col space-y-12 p-4 pb-8 text-foreground/80">
+          {!isLoading ?
+            data.messages.map((message) => (
+                <div key={message.id}>
+                    {
+                        message.type === 'question' ?
+                        (
+                            <div className="flex justify-end">
+                                <div className="group relative inline-block max-w-[80%] break-words rounded bg-background shadow-sm shadow-background p-4 text-left">
+                                    <div>{message.text}</div>
+                                </div>
+                            </div>
+                        )
+                    :
+                        (
+                            <div className="flex justify-start chat-content">
+                                <div className="group relative w-full max-w-full break-words">
+                                    <div className="space-y-4 prose prose-neutral prose-invert max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0">
+                                      <ReactMarkdown remarkPlugins={[remarkGfm]}>{message.text}</ReactMarkdown>
                                     </div>
                                 </div>
-                            )
-                        :
-                            (
-                                <div className="flex justify-start chat-content">
-                                    <div className="group relative w-full max-w-full break-words">
-                                        <div className="space-y-4 prose prose-neutral prose-invert max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0">
-                                          <ReactMarkdown remarkPlugins={[remarkGfm]}>{message.text}</ReactMarkdown>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        }
-                    </div>
-                )) : <div></div>
-              }
-              <div className="flex justify-start chat-content">
-                  <div className="group relative w-full max-w-full break-words">
-                      <div className="space-y-4 prose prose-neutral prose-invert max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0">
-                        <ReactMarkdown remarkPlugins={[remarkGfm]}>{output}</ReactMarkdown>
-                      </div>
+                            </div>
+                        )
+                    }
+                </div>
+            )) : <div></div>
+          }
+          <div className="flex justify-start chat-content">
+              <div className="group relative w-full max-w-full break-words">
+                  <div className="space-y-4 prose prose-neutral prose-invert max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0">
+                    <ReactMarkdown remarkPlugins={[remarkGfm]}>{output}</ReactMarkdown>
                   </div>
               </div>
-            </div>
           </div>
         </div>
-    
+      </div>
+    </div>
   );
 }
