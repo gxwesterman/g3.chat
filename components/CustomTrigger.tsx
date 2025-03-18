1dd50663c21d39071ddfd6be75fa7e978cd67456
Griffin Westerman <gxweste@gmail.com>
2025-03-18 12:19:28 -0500

    feat: continuing to better mimic T3

--- components/CustomTrigger.tsx
+++ components/CustomTrigger.tsx
@@ -12,7 +12,7 @@ export function CustomTrigger() {
   return (
     <button
       onClick={handleClick}
-      className={cn(!open && "rotate-180", "fixed flex items-center justify-center z-10 h-9 w-9 mt-3 ml-1 hover:bg-background/20 rounded-md transition-hover")}
+      className={cn(!open && "rotate-180", "fixed flex items-center justify-center z-10 h-9 w-9 mt-3 ml-1 hover:bg-secondary/20 rounded-md transition-hover")}
     >
       <ChevronLeft size="16px"/>
     </button>
