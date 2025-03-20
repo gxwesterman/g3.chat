3238ca7b6c47349a7ed80e17516658b3b6d6afa2
Griffin Westerman <gxweste@gmail.com>
2025-03-20 13:33:34 -0500

    fix: teeny tiny little itty bitty change to hover bg color

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -92,7 +92,7 @@ export function AppSidebar({ chats }: { chats: Chat[] }) {
                   className="hover:cursor-pointer hover:bg-sidebar-accent flex items-center justify-between"
                 >
                   <div className="truncate max-w-[75%] font-semibold text-muted-foreground">{`${chat.messages[0].text}`}</div>
-                  <button className="rounded-md p-1.5 hover:bg-destructive/50 hover:text-destructive-foreground absolute right-[-2rem] transition-all group-hover/item:right-1">
+                  <button className="rounded-md p-1.5 hover:bg-pink-800/50 hover:text-destructive-foreground absolute right-[-2rem] transition-all group-hover/item:right-1">
                     <X
                       className="h-4 w-4 text-muted-foreground"
                       onMouseDown={(e) => deleteChat(e, chat.id)}
