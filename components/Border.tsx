cf6726714ab583db7aaf45b7a5e6d3bf27c052a1
Griffin Westerman <gxweste@gmail.com>
2025-03-26 16:28:46 -0500

    fix: technically fixed minor visual bug

--- components/Border.tsx
+++ components/Border.tsx
@@ -10,10 +10,10 @@ export function Border() {
       <div className={cn(open ? "translate-y-3.5 rounded-tl-xl border-l border-t" : "translate-y-0 rounded-none border-none", "absolute bottom-0 top-0 w-full overflow-hidden border-border bg-background bg-fixed pb-[140px] transition-all ease-snappy")}>
         <div className="bg-noise absolute inset-0 -top-3.5 bg-fixed transition-transform ease-snappy [background-position:right_bottom]"></div>
       </div>
-      <div className={cn(open ? "border-b" : "border-none -translate-y-[15px]", "absolute inset-x-3 top-0 z-10 box-content h-3.5 overflow-hidden border-border gradient-noise-top backdrop-blur-md transition-[transform,border] ease-snappy")}>
+      <div className={cn(open ? "border-b" : "border-none -translate-y-[15px]", "absolute inset-x-3 top-0 z-10 box-content h-3.5 overflow-hidden border-border bg-gradient-noise-top backdrop-blur-md transition-[transform,border] ease-snappy")}>
         <div className="absolute left-0 top-0 h-full w-8 bg-gradient-to-r from-gradient-noise-top to-transparent"></div>
         <div className="absolute right-24 top-0 h-full w-8 bg-gradient-to-l from-gradient-noise-top to-transparent"></div>
-        <div className="absolute right-0 top-0 h-full w-24 gradient-noise-top"></div>
+        <div className="absolute right-0 top-0 h-full w-24 bg-gradient-noise-top"></div>
       </div>
       <div className="fixed right-0 top-0 z-20 h-16 w-28 max-sm:hidden">
         <div className={cn(!open && "-translate-y-3.5 scale-y-0", "group pointer-events-none absolute top-3.5 z-10 -mb-8 h-32 w-full origin-top transition-all ease-snappy")} style={{boxShadow: "10px -10px 8px 2px hsl(var(--gradient-noise-top))"}}>
