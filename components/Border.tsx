fb1c73a9db0d8400b23123d04ef5daabc322ef27
Griffin Westerman <gxweste@gmail.com>
2025-03-27 14:24:06 -0500

    feat: improved layout on mobile

--- components/Border.tsx
+++ components/Border.tsx
@@ -3,14 +3,14 @@ import { cn } from "@/lib/utils";
 import { ModeToggle } from "@/components/ModeToggle";
 
 export function Border() {
-  const { open } = useSidebar();
+  const { open, isMobile } = useSidebar();
 
   return (
     <>
-      <div className={cn(open ? "translate-y-3.5 rounded-tl-xl border-l border-t" : "translate-y-0 rounded-none border-none", "absolute bottom-0 top-0 w-full overflow-hidden border-border bg-background bg-fixed pb-[140px] transition-all ease-snappy")}>
+      <div className={cn(open && !isMobile ? "translate-y-3.5 rounded-tl-xl border-l border-t" : "translate-y-0 rounded-none border-none", "absolute bottom-0 top-0 w-full overflow-hidden border-border bg-background bg-fixed pb-[140px] transition-all ease-snappy")}>
         <div className="bg-noise absolute inset-0 -top-3.5 bg-fixed transition-transform ease-snappy [background-position:right_bottom]"></div>
       </div>
-      <div className={cn(open ? "border-b" : "border-none -translate-y-[15px]", "absolute inset-x-3 top-0 z-10 box-content h-3.5 overflow-hidden border-border bg-gradient backdrop-blur-md transition-[transform,border] ease-snappy")}>
+      <div className={cn(open && !isMobile ? "border-b" : "border-none -translate-y-[15px]", "absolute inset-x-3 top-0 z-10 box-content h-3.5 overflow-hidden border-border bg-gradient backdrop-blur-md transition-[transform,border] ease-snappy")}>
         <div className="absolute left-0 top-0 h-full w-8 bg-gradient-to-r from-sidebar-background/30 to-transparent"></div>
         <div className="absolute right-24 top-0 h-full w-8 bg-gradient-to-l from-gradient to-transparent"></div>
         <div className="absolute right-0 top-0 h-full w-24 bg-gradient"></div>
