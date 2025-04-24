a82c89851d1940eb2a85bea2e8ea236b1a61fe34
Griffin Westerman <gxweste@gmail.com>
2025-04-24 17:00:21 -0500

    added call to initDefaultPages in session setSession action

--- app/actions/setSession.ts
+++ app/actions/setSession.ts
@@ -1,14 +1,17 @@
 'use server'
 import { createSession } from '@/app/lib/session'
 import { id } from '@instantdb/react';
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
+import { initDefaultPages } from '@/app/lib/instant';
 
 export async function setSession() {
   try {
-    const cookieStore = await cookies()
+    const cookieStore = await cookies();
     const session = cookieStore.get('session');
+    const sessionId = id();
     if (!session) {
-      await createSession(id());
+      await initDefaultPages(sessionId);
+      await createSession(sessionId);
     }
   } catch (error) {
     console.log(error);
