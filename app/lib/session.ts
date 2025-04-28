489b2ae9be76c753255be5ac6e8dd1a4d4956993
Griffin Westerman <gxweste@gmail.com>
2025-04-28 14:16:31 -0500

    added a semicolon, slay

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
  
 export async function createSession(sessionId: string) {
   const expiresAt = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000)
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
 
   cookieStore.set('session', sessionId, {
     httpOnly: false,
