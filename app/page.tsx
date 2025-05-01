668efea3d3d1e2437202ea65b0ff1240aa7f854f
Griffin Westerman <gxweste@gmail.com>
2025-05-01 14:38:07 -0500

    pages do nothing

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,3 @@
-'use client'
-import { setSession } from '@/app/actions/setSession'
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
- 
 export default function Page() {
-  const router = useRouter();
-  useEffect(() => {
-    const login = async () => {
-      const status = await setSession();
-      if (status) {
-        router.push('/chat/welcome');
-      } else {
-        router.push('/chat');
-      }
-    }
-    login();
-  }, []);
+ return <></>
 }
